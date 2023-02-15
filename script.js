434724f7c2595c05d8a18de36e2c96bb107d357c
Rafael Mileris <rafaelmileris15@gmail.com>
2023-02-15 00:46:48 -0300

    Adicionando os comandos para o 'Tema Escuro'

--- script.js
+++ script.js
@@ -17,7 +17,20 @@ btn_adicionar.addEventListener("click", ()=>{
 //* ALTERAR O TEMA
 let btn_tema = document.getElementById("btn-tema")
 let bloco_principal = document.getElementById("principal")
+let btn_add =  btn_adicionar
+let btn_alterar = document.getElementById("btn-alt")
+let btn_deletar = document.getElementById("btn-del")
+let btn_listar = document.getElementById("btn-lst")
+let btn_alterar_icone = document.getElementById("btn-alt-icone")
+let tarefas = document.getElementById("tela-escuro")
 
 btn_tema.addEventListener("click", ()=>{
     bloco_principal.classList.toggle("tema-escuro")
+    tarefas.classList.toggle("tela-escuro")
+    btn_add.classList.toggle("botoes-escuro")
+    btn_alterar.classList.toggle("botoes-escuro")
+    btn_deletar.classList.toggle("botoes-escuro")
+    btn_listar.classList.toggle("botoes-escuro")
+    btn_alterar_icone.classList.toggle("botoes-escuro")
+    btn_tema.classList.toggle("botoes-claro")
 })
