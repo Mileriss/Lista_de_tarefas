acb7c38ed2a5a6f5b4207492e9cabb5f10b72ec4
Rafael Mileris <rafaelmileris15@gmail.com>
2023-02-19 03:03:01 -0300

    Resolvendo uma parte do problema

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //* ADICIONAR TAREFAS
+const tela_tarefa = document.getElementById("lista")
 let add_tarefa = document.getElementById("campo-add")
 let btn_adicionar = document.getElementById("btn-add")
-const tela_tarefa = document.getElementById("lista")
 
 btn_adicionar.addEventListener("click", ()=>{
     const novo_elemento = document.createElement("li")
-    novo_elemento.setAttribute("class", "tarefas tar-sel")
+    novo_elemento.setAttribute("class", "tarefas")
     novo_elemento.innerHTML = add_tarefa.value
     if(add_tarefa.value == ""){
         alert("O campo não pode ficar vazio!") 
@@ -21,7 +21,6 @@ tarefas_armazenadas.map((elemento)=>{
     elemento.addEventListener("click", (selecionado)=>{
         const tarefa_sel = selecionado.target
         tarefa_sel.classList.toggle("tar-sel")
-
     })
 })
 let btn_del = document.getElementById("btn-del")
@@ -38,6 +37,7 @@ let bloco_principal = document.getElementById("principal")
 let tarefas = document.getElementById("tela-escuro")
 let btn_tema = document.getElementById("btn-tema")
 const lista_btn = [...document.getElementsByClassName("botoes")]
+// const lista_tarefas = [...document.getElementsByClassName("tarefas")]
 
 btn_tema.addEventListener("click", ()=>{
     bloco_principal.classList.toggle("tema-escuro")
@@ -46,4 +46,5 @@ btn_tema.addEventListener("click", ()=>{
     for(btn of lista_btn){
         btn.classList.toggle("botoes-escuro")
     }
+
 })
